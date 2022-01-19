50213a2d44f7014c4bb3599ceec077883fd80ad6
markanthony999 <50175467+markanthony999@users.noreply.github.com>
2022-01-19 06:53:54 -0600

    Initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,63 @@
+import NextApp from 'next/app';
+
+import { SiteContext, useSiteContext } from 'hooks/use-site';
+import { SearchProvider } from 'hooks/use-search';
+
+import { getSiteMetadata } from 'lib/site';
+import { getRecentPosts } from 'lib/posts';
+import { getTopLevelPages } from 'lib/pages';
+import { getCategories } from 'lib/categories';
+import NextNProgress from 'nextjs-progressbar';
+import { getAllMenus, createMenuFromPages, MENU_LOCATION_NAVIGATION_DEFAULT } from 'lib/menus';
+
+import 'styles/globals.scss';
+import variables from 'styles/_variables.module.scss';
+
+function App({ Component, pageProps = {}, metadata, recentPosts, categories, menus }) {
+  const site = useSiteContext({
+    metadata,
+    recentPosts,
+    categories,
+    menus,
+  });
+
+  return (
+    <SiteContext.Provider value={site}>
+      <SearchProvider>
+        <NextNProgress height={4} color={variables.progressbarColor} />
+        <Component {...pageProps} />
+      </SearchProvider>
+    </SiteContext.Provider>
+  );
+}
+
+App.getInitialProps = async function (appContext) {
+  const appProps = await NextApp.getInitialProps(appContext);
+
+  const { posts: recentPosts } = await getRecentPosts({
+    count: 5,
+  });
+
+  const { categories } = await getCategories({
+    count: 5,
+  });
+
+  const { menus } = await getAllMenus();
+
+  const defaultNavigation = createMenuFromPages({
+    locations: [MENU_LOCATION_NAVIGATION_DEFAULT],
+    pages: await getTopLevelPages(),
+  });
+
+  menus.push(defaultNavigation);
+
+  return {
+    ...appProps,
+    metadata: await getSiteMetadata(),
+    recentPosts,
+    categories,
+    menus,
+  };
+};
+
+export default App;
